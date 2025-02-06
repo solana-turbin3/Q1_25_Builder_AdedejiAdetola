00eacaf9760c45fbacc2dca0cd96329456810dc2
Adetola Adedeji <adetolaadedeji0@gmail.com>
2025-02-06 09:54:40 +0100

    Complete Anchor Amm

---
+++ anchor_amm/programs/anchor_amm/src/instructions/swap.rs
@@ -0,0 +1,127 @@
+use crate::state::Config;
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    self,
+    associated_token::AssociatedToken,
+    token::{transfer, Mint, Token, TokenAccount, Transfer},
+};
+use constant_product_curve::ConstantProduct;
+use constant_product_curve::LiquidityPair;
+
+#[derive(Accounts)]
+#[instruction(seed: u64)]
+pub struct Swap<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub mint_x: Account<'info, Mint>,
+    pub mint_y: Account<'info, Mint>,
+    #[account(
+        mut,
+        associated_token::mint = mint_x,
+        associated_token::authority = config,
+    )]
+    pub vault_x: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint_x,
+        associated_token::authority = user,
+    )]
+    pub user_x: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint_y,
+        associated_token::authority = config,
+    )]
+    pub vault_y: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint_y,
+        associated_token::authority = user,
+    )]
+    pub user_y: Account<'info, TokenAccount>,
+    #[account(
+        has_one = mint_x,
+        has_one = mint_y,
+        seeds = [b"config", config.seed.to_le_bytes().as_ref()],
+        bump = config.config_bump,
+    )]
+    pub config: Account<'info, Config>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+impl<'info> Swap<'info> {
+    pub fn swap(&mut self, is_x: bool, amount: u64, min: u64) -> Result<()> {
+        let mut curve = ConstantProduct::init(
+            self.vault_x.amount,
+            self.vault_y.amount,
+            self.vault_x.amount,
+            self.config.fee,
+            None,
+        )
+        .unwrap();
+        let p = match is_x {
+            true => LiquidityPair::X,
+            false => LiquidityPair::Y,
+        };
+        let res = curve.swap(p, amount, min).unwrap();
+
+        self.deposit_tokens(is_x, res.deposit).unwrap();
+        self.withdraw_tokens(is_x, res.withdraw).unwrap();
+        Ok(())
+    }
+
+    pub fn deposit_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
+        let (from, to) = match is_x {
+            true => (
+                self.user_x.to_account_info(),
+                self.vault_x.to_account_info(),
+            ),
+            false => (
+                self.user_y.to_account_info(),
+                self.vault_y.to_account_info(),
+            ),
+        };
+
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_account = Transfer {
+            from,
+            to,
+            authority: self.user.to_account_info(),
+        };
+        let ctx = CpiContext::new(cpi_program, cpi_account);
+        transfer(ctx, amount)?;
+        Ok(())
+    }
+    pub fn withdraw_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
+        let (from, to) = match is_x {
+            true => (
+                self.vault_x.to_account_info(),
+                self.user_x.to_account_info(),
+            ),
+            false => (
+                self.vault_y.to_account_info(),
+                self.user_y.to_account_info(),
+            ),
+        };
+
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_account = Transfer {
+            from,
+            to,
+            authority: self.config.to_account_info(),
+        };
+        let seeds = &[
+            &b"config"[..],
+            &self.config.seed.to_le_bytes(),
+            &[self.config.config_bump],
+        ];
+
+        let signer_seeds = &[&seeds[..]];
+        let ctx = CpiContext::new_with_signer(cpi_program, cpi_account, signer_seeds);
+        transfer(ctx, amount)?;
+        Ok(())
+    }
+}
---
+++ anchor_amm/programs/anchor_amm/src/lib.rs
@@ -0,0 +1,43 @@
+use anchor_lang::prelude::*;
+mod instructions;
+mod state;
+
+use crate::instructions::*;
+// use crate::state::*;
+
+declare_id!("7RRexRSvtJYqY75W6rohaHByADHsA3K8vXod6iJWb52P");
+
+#[program]
+pub mod anchor_amm {
+    use crate::instructions::{Deposit, Initialize, Swap};
+
+    use super::*;
+
+    pub fn init(
+        ctx: Context<Initialize>,
+        seed: u64,
+        fee: u16,
+        authority: Option<Pubkey>,
+    ) -> Result<()> {
+        ctx.accounts.init(seed, fee, authority, &ctx.bumps)?;
+        Ok(())
+    }
+    pub fn deposit(
+        ctx: Context<Deposit>,
+        amount: u64,
+        max_x: u64,
+        max_y: u64,
+        is_x: bool,
+    ) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y)?;
+        ctx.accounts.deposit_tokens(is_x, amount)?;
+        ctx.accounts.mint_lp(amount)?;
+        Ok(())
+    }
+    pub fn swap(ctx: Context<Swap>, is_x: bool, amount: u64, min: u64) -> Result<()> {
+        ctx.accounts.swap(is_x, amount, min)?;
+        ctx.accounts.deposit_tokens(is_x, amount)?;
+        ctx.accounts.withdraw_tokens(is_x, amount)?;
+        Ok(())
+    }
+}
